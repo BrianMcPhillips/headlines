a4782d95178c06397d9a86bcadda58a2836205ea
Brian McPhillips <B.McPhillips89@gmail.com>
2022-01-20 14:51:47 -0800

    Items rendered to screen with API call

--- src/containers/Headlines.jsx
+++ src/containers/Headlines.jsx
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
+import ArticleList from '../components/ArticleList/ArticleList';
+import Loading from '../components/Loading/Loading';
 
 export default class Headlines extends Component {
   state = {
-    data: [],
-    searchTerm: ''
+    articles: [],
+    loading: true
+  }
+
+  componentDidMount = () => {
+    fetch(`https://newsapi.org/v2/everything?q=America&apiKey=${process.env.REACT_APP_TOKEN}`)
+      .then(res => res.json())
+      .then(json => json.articles.map(article => ({
+        urlToImage: article.urlToImage,
+        title: article.title,
+        description: article.description,
+        author: article.author,
+        url: article.url
+      })))
+      .then(articles => this.setState({ articles, loading: false })); 
   }
   render() {
+    const { articles, loading } = this.state;
+    if(loading) return <Loading />;
     return (
       <div>
-        
+        <ArticleList data={articles}/>
       </div>
     );
   }
