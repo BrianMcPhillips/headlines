d4b92bfec6d2eb88e3ec6979988606a20001da6f
Brian McPhillips <B.McPhillips89@gmail.com>
2022-01-19 17:27:20 -0800

    Loading Gif with Test

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => (
+  <div>
+    <img 
+      src="https://www.animatedimages.org/data/media/152/animated-newspaper-image-0026.gif" 
+      alt="Loading" />
+  </div>
+);
+
+export default Loading;
---
+++ src/containers/Headlines.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Headlines extends Component {
+  state = {
+    data: [],
+    searchTerm: ''
+  }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
