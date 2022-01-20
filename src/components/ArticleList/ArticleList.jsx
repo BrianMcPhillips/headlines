0cde85ca8da6a5ed4b51c5894e68c263cdc846cd
Brian McPhillips <B.McPhillips89@gmail.com>
2022-01-19 17:05:24 -0800

    Article list with test

---
+++ src/components/ArticleList/ArticleList.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Article from '../Article/Article';
+
+const ArticleList = ({ data }) => {
+  const articleElements = data.map(article => (
+    <li key={article.url}>
+      <Article {...article}/>
+    </li>
+  ));
+  return (
+    <ul>
+      {articleElements }
+    </ul>
+  );
+};
+
+ArticleList.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape({
+    urlToImage: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired
+  })).isRequired
+};
+
+export default ArticleList;
